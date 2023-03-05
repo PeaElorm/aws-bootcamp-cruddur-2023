83c2f25d768c54435edbc31fb2ffab70cdaf224a
Perfect Elorm Avugla <elormavugla7@gmail.com>
2023-03-05 19:24:45 +0000

    home activities edit

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,7 @@ class HomeActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
+      'handle':  'Perfect Elorm',
       'message': 'Cloud is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
@@ -14,8 +14,8 @@ class HomeActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'handle':  'Seven',
+        'message': 'Live. love. Laugh. Code
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
